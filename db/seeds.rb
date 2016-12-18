74449f61c8e637c8d98a9d04a71b5713bbb7ca6d
Sam Raife <swraife@gmail.com>
2016-12-18 15:23:37 -0700

    Adds db seed data for users and songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'creating users'
+names = [%w(Jet Li), %w(John Wayne), %w(Lucy Lawless)]
+names.each do |first, last|
+  User.where(email: "#{first.downcase}@#{last.downcase}.com").first_or_create(
+    first_name: first,
+    last_name: last,
+    password_confirmation: 'password',
+    password: 'password',
+  )
+end
+puts 'users created'
+
+puts 'creating songs'
+song_names = ['Cherokee Shuffle', 'Summertime', 'Golden Slippers']
+song_names.each do |name|
+  Song.where(name: name).first_or_create(name: name, user: User.first)
+end
+puts 'songs created'
