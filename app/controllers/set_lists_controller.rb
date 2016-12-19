f78359f8ce86b093ff7789c73fe918066e6284d5
Sam Raife <swraife@gmail.com>
2016-12-19 00:45:29 -0700

    adds create set_lists controller action

--- app/controllers/set_lists_controller.rb
+++ app/controllers/set_lists_controller.rb
@@ -2,4 +2,16 @@ class SetListsController < ApplicationController
   def new
     @set_list = SetList.new
   end
+
+  def create
+    @set_list = SetList.create(set_list_params)
+    @set_list.users << current_user
+    redirect_to 'root'
+  end
+
+  private
+
+  def set_list_params
+    params.require(:set_list).permit(:name, :description)
+  end
 end
