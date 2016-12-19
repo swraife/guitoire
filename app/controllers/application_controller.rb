6a468d27d3c363ecc68366a5258ca32c37d6e46a
Sam Raife <swraife@gmail.com>
2016-12-19 00:45:04 -0700

    require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :current_user_or_initialize
-
-  def current_user_or_initialize
-    @current_user = current_user || User.new
-  end
+  before_action :authenticate_user!
 end
