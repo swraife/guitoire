ef0d863fa2a8cedb4955afab0f9dbfee1c48d855
Sam Raife <swraife@gmail.com>
2017-01-16 12:06:24 -0700

    add service to create jim_songs and their resources

---
+++ app/services/jim_song_creator.rb
@@ -0,0 +1,28 @@
+class JimSongCreator
+  def self.create_songs
+    entries = Dir.entries('public/jim_songs')
+      .delete_if { |item| ['.','..'].include?(item) }
+
+    jim_song_path = Rails.root.join('public/jim_songs/')
+
+    ActiveRecord::Base.transaction do
+      entries.each do |file_name|
+        puts "creating #{file_name}"
+        song = Song.where(user_id: 2, name: file_name.split('.')[0])
+                   .first_or_initialize
+        song.persisted? ? next : song.save!
+
+        path = jim_song_path + file_name
+        file_resource = FileResource.new(main: File.new(path, 'r'))
+
+        SongResourceCreator.new(
+          song_id: song.id, resourceable: file_resource
+        ).create
+      end
+    end
+
+  rescue ActiveRecord::ActiveRecordError => exception
+    @errors = exception
+    puts @errors    
+  end
+end
