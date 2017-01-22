5d8a4f3d45da7aaf811df2d5f3917359e120043f
Sam Raife <swraife@gmail.com>
2017-01-22 14:24:25 -0700

    rubocop style changes

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,50 +4,58 @@ FactoryGirl.define do
     set_list_id 1
     role 1
   end
+
   factory :note_resource do
-    
   end
+
   factory :url_resource do
-    
   end
+
   factory :file_resource do
-    
   end
+
   factory :set_list_song do
     song_id 1
     set_list_id 1
-    music_key "MyString"
+    music_key 'MyString'
   end
+
   factory :set_list do
-    name "MyString"
-    description "MyString"
+    name 'MyString'
+    description 'MyString'
   end
+
   factory :event do
-    name "MyString"
-    date "2016-12-18"
+    name 'MyString'
+    date '2016-12-18'
     set_list_id 1
   end
+
   factory :song_resource do
     song_id 1
     resource_id 1
   end
+
   factory :resource do
-    name "MyString"
+    name 'MyString'
   end
+
   factory :composer do
-    name "MyString"
+    name 'MyString'
   end
+
   factory :song do
-    name "MyString"
-    description "MyText"
-    music_key "MyString"
+    name 'MyString'
+    description 'MyText'
+    music_key 'MyString'
     tempo 1
     composer_id 1
   end
+
   factory :user do
-    first_name "MyString"
-    last_name "MyString"
-    sequence(:email) { |n| "email#{n}@test.org"}
+    first_name 'MyString'
+    last_name 'MyString'
+    sequence(:email) { |n| "email#{n}@test.org" }
     password 'password'
     password_confirmation 'password'
   end
