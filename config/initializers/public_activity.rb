dbf806d177e330876a462fbee80b36403ae90740
Sam Raife <swraife@gmail.com>
2017-03-04 16:27:35 -0700

    group resource.create activities by song

--- config/initializers/public_activity.rb
+++ config/initializers/public_activity.rb
@@ -2,6 +2,8 @@ PublicActivity::Activity.class_eval do
   def group
     group = [owner, created_at.yday, key]
     group.push(recipient.creator) if key == 'song_role.create_follower'
+    group.push(recipient) if key == 'resource.create'
+
     group
   end
 end
