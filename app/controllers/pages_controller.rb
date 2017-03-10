5384c5c3c1363a179fe04cfcfd0cc4bfe42bc779
Sam Raife <swraife@gmail.com>
2017-03-09 23:19:04 -0700

    adds eager loading in pages_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
-    @activities = PublicActivity::Activity.includes(:trackable, :recipient, :owner).order(created_at: :desc).page(1)
+    @activities = PublicActivity::Activity.includes(:trackable, :owner, recipient: :creator).order(created_at: :desc).page(1)
     @activity_grouper = ActivityGrouper.new(@activities)
   end
 end
