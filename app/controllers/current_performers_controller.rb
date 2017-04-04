fcb888e35c62f1c5dcb41766ad4527b3b88103fa
Sam Raife <swraife@gmail.com>
2017-04-03 20:46:07 -0600

    redirect back to same page after changing current_performer.

--- app/controllers/current_performers_controller.rb
+++ app/controllers/current_performers_controller.rb
@@ -1,6 +1,6 @@
 class CurrentPerformersController < ApplicationController
   def create
     session[:performer_id] = current_user.performers.find(params[:performer_id]).id
-    redirect_to '/'
+    redirect_back(fallback_location: '/')
   end
 end
