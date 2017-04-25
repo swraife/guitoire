e753704b04d5cce9cc11adf340416efe9da53b80
Sam Raife <swraife@gmail.com>
2017-04-24 19:33:51 -0600

    when switching current_performer, it redirects if request.referrer path includes 'performers/previous_current_performer_id'.

--- app/controllers/current_performers_controller.rb
+++ app/controllers/current_performers_controller.rb
@@ -1,6 +1,10 @@
 class CurrentPerformersController < ApplicationController
   def create
     session[:performer_id] = current_user.performers.find(params[:performer_id]).id
-    redirect_back(fallback_location: '/')
+    if request.referrer.include? "performers/#{current_performer.id}"
+      redirect_to request.referrer.gsub(current_performer.id.to_s, params[:performer_id])
+    else
+      redirect_back(fallback_location: '/')
+    end
   end
 end
