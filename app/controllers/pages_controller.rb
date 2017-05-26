6d8ff7a14f579a590604608156c102c6dc6f4753
Sam Raife <swraife@gmail.com>
2017-05-26 16:22:59 -0600

    Language changes. Fixes minor bug in search. Changes activity pagination per to 35.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
     @activities = Query::ActivityService.new(current_performer)
                     .dashboard_activities
-                    .order(created_at: :desc).page(params[:page])
+                    .order(created_at: :desc).page(params[:page]).per(35)
     @activity_grouper = ActivityGrouper.new(@activities)
   end
 end
